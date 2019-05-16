d139f775c9da96c656422145a7b517907cb989a4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-16 13:48:03 -0600

    Add test for list of users on the user index

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "As a visitor", type: :feature do
   describe "when I visit the user index page" do
+    it "shows a list of all users and their emails" do
+      user_1 = User.create!(name: "name one", email: "my email", password: "password123")
+      user_2 = User.create!(name: "name two", email: "your email", password: "password12345")
+
+      visit users_path
+
+      within("#user-#{user_1.id}") do
+        expect(page).to have_content(user_1.name)
+        expect(page).to have_content(user_1.email)
+      end
+
+      within("#user-#{user_2.id}") do
+        expect(page).to have_content(user_2.name)
+        expect(page).to have_content(user_2.email)
+      end
+    end
+
     it "has a link to register a new user" do
       visit users_path
       click_link "Register as User"
