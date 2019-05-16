db8d55e6548a6ebe441be0c1c0ed84f514db7913
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-16 15:56:31 -0600

    Add model tests of User::authenicate

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,22 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password }
     it { should validate_uniqueness_of :email }
   end
+
+  describe "class methods" do
+    it "authenticate returns a user if there is a matching user and password" do
+      name = "bob"
+      email = "bob@example.com"
+      password = "pw123"
+      user = User.create!(name: name, email: email, password: password)
+      expect(User.authenticate(email, password)).to eq(user)
+    end
+
+    it "authenticate returns nil if there is no matching user and password" do
+      name = "bob"
+      email = "bob@example.com"
+      password = "pw123"
+      user = User.create!(name: name, email: email, password: password)
+      expect(User.authenticate(email, "wrong")).to eq(nil)
+    end
+  end
 end
