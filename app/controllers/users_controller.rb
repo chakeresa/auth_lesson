146a6d25dba182bf56d82ef4437a9a575b51ad50
Alexandra Chakeres <alexchakeres@gmail.com>
2019-05-16 14:48:55 -0600

    Add link to new session / login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  private
+
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
   validates_presence_of :name, :email, :password
   validates_uniqueness_of :email
+
+  def self.authenticate(email, password)
+    # if email and password correspond to a valid user, return that user
+    # otherwise, return nil
+  end
 end
